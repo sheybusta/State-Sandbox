3a7b08ca23be0815d6c80d6c8ebf5b9888dad225
Sheyla <shel.busta@gmail.com>
2021-06-23 20:19:57 +0200

    art wall component

---
+++ art-wall/src/component/compornents.js
@@ -0,0 +1,24 @@
+const artWall = (height, width) => {
+  // parameters same as keys of state
+  //
+  // can you figure out how to write this component with classes instead of .style?
+  const table = document.createElement("table");
+  table.className = "wall";
+
+  for (let i = 0; i < height; i++) {
+    const row = document.createElement("tr"); //
+    row.style.height = `${100 / height}%`;
+
+    for (let j = 0; j < width; j++) {
+      const changer = colorChanger("wall-colorr");
+
+      const cell = document.createElement("td");
+      cell.style.width = `${100 / width}%;`;
+      cell.appendChild(changer);
+
+      row.appendChild(cell); //<tr><td></td></tr>
+    }
+    table.appendChild(row);
+  }
+  return table;
+};
